9b37618d1043bd7f81670ebdc03eef65c3b60b54
kosmos <kosmos.by@gmail.com>
2015-07-23 12:10:09 +0300

    changed config files

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
     hosts: {
-        domain: 'jitsi-meet.example.com',
+        domain: 'conf.vri-edimart.com',
         //anonymousdomain: 'guest.example.com',
-        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.jitsi-meet.example.com', // FIXME: use XEP-0030
+        muc: 'conference.conf.vri-edimart.com', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.conf.vri-edimart.com', // FIXME: use XEP-0030
         //jirecon: 'jirecon.jitsi-meet.example.com',
         //call_control: 'callcontrol.jitsi-meet.example.com',
         //focus: 'focus.jitsi-meet.example.com' - defaults to 'focus.jitsi-meet.example.com'
@@ -12,11 +12,11 @@ var config = {
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//conf.vri-edimart.com:8088/http-bind', // FIXME: use xep-0156 for that
     clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
     //focusUserJid: 'focus@auth.jitsi-meet.example.com', // The real JID of focus participant - can be overridden here
     //defaultSipNumber: '', // Default SIP number
-    desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
+    desktopSharing: false, // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
     chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
     desktopSharingSources: ['screen', 'window'],
     minChromeExtVersion: '0.1', // Required version of Chrome extension
